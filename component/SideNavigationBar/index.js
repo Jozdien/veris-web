e078abb46287974fb2d34c41acf4584335e9bff6
Jozdien <joseverres42@gmail.com>
2020-11-26 23:26:12 +0530

    Final Changes for the IEEE UI Design Challenge

--- component/SideNavigationBar/index.js
+++ component/SideNavigationBar/index.js
@@ -5,6 +5,49 @@ export default function NavigationBar({selected}) {
     <div className={styles.container}>
       <div className={styles.topspace}></div>
       <a href="404" className={styles.iconbox}>
+        <img src="/Logo.png" className={styles.img}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/home" id="home" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/gradcap" id="marks_attendance" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/rupee" id="fees" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/calendar" id="events" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/blog" id="blogs" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/news" id="news" selected={selected}/>
+      </a>
+      <div className={styles.gap}></div>
+      <a href="404" className={styles.iconbox}>
+        <Icon imageSource="/icons/bell" id="notifications" selected={selected}/>
+      </a>
+      <div className={styles.bottomspace}></div>
+    </div>
+  )
+}
+
+/* 
+import styles from './styles.module.css'
+import Icon from '../IconNavigationBar'
+export default function NavigationBar({selected}) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.topspace}></div>
+      <a href="404" className={styles.iconbox}>
         <Icon imageSource="/icons/logo" id="vercel"/>
       </a>
       <div className={styles.gap}></div>
@@ -39,3 +82,4 @@ export default function NavigationBar({selected}) {
     </div>
   )
 }
+*/
